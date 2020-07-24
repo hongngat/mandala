78a8aede5dcf76a0d016e16f7f67229681fcc7f2
hongngat <48482193+hongngat@users.noreply.github.com>
2020-07-24 14:44:50 +0700

    n

--- assets/js/madala.js
+++ assets/js/madala.js
@@ -256,6 +256,12 @@ $(document).ready(function() {
     $('#datepicker-1').datepicker({
         uiLibrary: 'bootstrap4'
     });
+    $('#datepicker-2').datepicker({
+        uiLibrary: 'bootstrap4'
+    });
+    $('#datepicker-3').datepicker({
+        uiLibrary: 'bootstrap4'
+    });
     $('.pagination li').click(function() {
         $(this).addClass('active').siblings().removeClass('active');
     });
@@ -485,3 +491,30 @@ function fmask_select_close() {
 }
 
 //end select scroll Ưu đãi
+
+//form booking mobile
+// Get the modal
+var modal = document.getElementById("booking_mobile_ID");
+
+// Get the button that opens the modal
+var btn = document.getElementById("link_booking_hotel");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
